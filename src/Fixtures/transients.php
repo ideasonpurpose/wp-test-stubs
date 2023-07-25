6a7c62a929ffb631a6235892c1f7ac8376d32db2
Joe Maller <joe@joemaller.com>
2023-07-25 15:47:22 -0400

    refactor transients stubs, add delete_transient()

--- src/Fixtures/transients.php
+++ src/Fixtures/transients.php
@@ -1,23 +1,44 @@
 <?php
 
 /**
- * @link https://developer.wordpress.org/reference/functions/get_transient
+ * NOTE: Updated 2023-07-25
+ *
+ * All three functions work the same way:
+ * A global $transients array is appended with an array containing the function name and all arguments
+ * Each function returns a named global matching its name, cast to a boolean value.
  */
-function get_transient($id)
+
+function transientHelper($function, $args, $return)
 {
     global $transients;
-    $transients ??= [];
-    if (array_key_exists($id, $transients)) {
-        return $transients[$id];
-    }
-    return false;
+    $transients = is_array($transients) ? $transients : [];
+    $transients[] = array_merge([$function], $args);
+    return (bool) $return;
+}
+
+/**
+ * @link https://developer.wordpress.org/reference/functions/get_transient
+ */
+function get_transient(string $transient): mixed
+{
+    global $get_transient;
+    return transientHelper('get', func_get_args(), $get_transient);
 }
 
 /**
  * @link https://developer.wordpress.org/reference/functions/set_transient
  */
-function set_transient($data, $timeout)
+function set_transient(string $transient, mixed $value, int $expiration): bool
 {
     global $set_transient;
-    $set_transient[] = ['data' => $data, 'timeout' => $timeout];
+    return transientHelper('set', func_get_args(), $set_transient);
+}
+
+/**
+ * @link https://developer.wordpress.org/reference/functions/delete_transient
+ */
+function delete_transient(string $transient): bool
+{
+    global $delete_transient;
+    return transientHelper('delete', func_get_args(), $delete_transient);
 }
