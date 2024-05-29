bdc96e0192653b1ba861e2e670b8371a84474db6
Joe Maller <joe@joemaller.com>
2024-05-29 17:18:06 -0400

    add get_post_type

---
+++ src/Fixtures/functions/get_post_type.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @link https://developer.wordpress.org/reference/functions/get_post_type/
+ */
+function get_post_type($post = null): string|false
+{
+    global $post_type;
+    return $post_type ?? false;
+}
