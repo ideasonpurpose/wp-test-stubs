5283e87d6133500f800ab5ea469125f1a3c5c082
Joe Maller <joe@joemaller.com>
2026-07-05 21:16:24 -0400

    Replace iterators with a flat list of requires

--- src/Stubs.php
+++ src/Stubs.php
@@ -14,13 +14,39 @@ class Stubs
     {
         self::setupGlobals();
 
-        $dir = __DIR__ . '/Fixtures';
-        $iterator = new \RecursiveDirectoryIterator($dir);
-        foreach (new \RecursiveIteratorIterator($iterator) as $file) {
-            if (strtolower($file->getExtension()) === 'php') {
-                require_once $file;
-            }
-        }
+        $fixtures = __DIR__ . '/Fixtures';
+
+        require_once "$fixtures/classes.php";
+        require_once "$fixtures/classes/WP_Image_Editor.php";
+        require_once "$fixtures/classes/WP_List_Table.php";
+        require_once "$fixtures/classes/WP_Post_Type.php";
+        require_once "$fixtures/classes/WP_REST_Controller.php";
+        require_once "$fixtures/classes/WP_REST_Posts_Controller.php";
+        require_once "$fixtures/classes/WP_REST_Request.php";
+        require_once "$fixtures/classes/WP_REST_Server.php";
+        require_once "$fixtures/classes/WP_Taxonomy.php";
+
+        require_once "$fixtures/functions/get_object_taxonomies.php";
+        require_once "$fixtures/functions/get_post_type.php";
+        require_once "$fixtures/functions/get_post.php";
+        require_once "$fixtures/functions/get_posts.php";
+        require_once "$fixtures/functions/get_the_terms.php";
+        require_once "$fixtures/functions/wp_list_pluck.php";
+
+        require_once "$fixtures/acf.php";
+        require_once "$fixtures/assets.php";
+        require_once "$fixtures/attachments.php";
+        require_once "$fixtures/constants.php";
+        require_once "$fixtures/copies.php";
+        require_once "$fixtures/hooks.php";
+        require_once "$fixtures/i18n.php";
+        require_once "$fixtures/is_.php";
+        require_once "$fixtures/plugin.php";
+        require_once "$fixtures/query_var.php";
+        require_once "$fixtures/screen.php";
+        require_once "$fixtures/stubs.php";
+        require_once "$fixtures/transients.php";
+        require_once "$fixtures/urls.php";
     }
 
     /**
@@ -48,6 +74,6 @@ class Stubs
     public static function error_log($err)
     {
         global $error_log;
-        $error_log = "{(string) $error_log}\n{$err}";    
+        $error_log = "{(string) $error_log}\n{$err}";
     }
 }
